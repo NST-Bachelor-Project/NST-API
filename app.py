68493895316c0e3447af1a89efc60ff1306c9fc4
demetre <durid17@freeuni.edu.ge>
2021-06-15 15:23:52 +0400

    CORS added

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
+from flask_cors import CORS
 import base64
 from nst import *
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/',  methods=['POST'])
 def NST_reqeust():
--- nst.py
+++ nst.py
@@ -1,7 +1,7 @@
-
+import base64
 
 def NST(style_img, content_img):
     print(type(style_img))
     result = {}
-    result["image"] = 'sucess'
+    result["image"] = 'data:image/jpg;base64,' + base64.b64encode(style_img).decode('utf-8')
     return result
